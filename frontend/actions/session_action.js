3bec60f4208a53be7114d05f553dbaee50ef033d
learnor <learnor@gmail.com>
2018-01-31 15:27:27 -0800

    implement session_action

---
+++ frontend/actions/session_action.js
@@ -0,0 +1,26 @@
+import {
+  postUser,
+  postSession,
+  deleteSession
+} from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+
+export const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user,
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+export const signup = formUser => dispatch => postUser(formUser)
+  .then(user => dispatch(receiveCurrentUser(user)));
+
+export const login = formUser => dispatch => postSession(formUser)
+  .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => deleteSession()
+  .then(() => dispatch(logoutCurrentUser()));
