7e8ee401c39103ab1f54b90c6910ee464296593c
learnor <learnor@gmail.com>
2018-02-01 12:06:43 -0800

    protect fron-end routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session/session_form_container';
-import { Route } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
   <div>
@@ -10,8 +10,8 @@ const App = () => (
       <GreetingContainer />
     </header>
 
-    <Route path="/login" component={SessionFormContainer} />
-    <Route path="/signup" component={SessionFormContainer} />
+    <AuthRoute path="/login" component={SessionFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
 );
 
