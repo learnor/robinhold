450fb276ac64954235eab69a891010f9c9ae9dbf
learnor <learnor@gmail.com>
2018-01-31 16:57:08 -0800

    fix session_errors_reducer: add initial state

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -5,7 +5,7 @@ import {
 
 import { merge } from 'lodash';
 
-export default (state, action) => {
+export default (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
