5d374db4e21ab946f1126e63e40749b4c9e26ac6
learnor <learnor@gmail.com>
2018-02-04 11:46:38 -0800

    add protected path to route_util

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -3,8 +3,18 @@ import { connect } from 'react-redux';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 
 const Auth = ({component: Component, path, loggedIn, exact}) => (
-  <Route path={path} exact={exact} render={(props) => (
+  <Route path={path} render={(props) => (
     !loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/" />
+    )
+  )} />
+);
+
+const Protected = ({component: Component, path, loggedIn}) => (
+  <Route path={path} render={props => (
+    loggedIn ? (
       <Component {...props} />
     ) : (
       <Redirect to="/" />
@@ -16,4 +26,16 @@ const mapStateToProps = state => {
   return {loggedIn : Boolean(state.session.currentUser)};
 };
 
-export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+export const AuthRoute = withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(Auth)
+);
+
+export const ProtectedRoute = withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(Protected)
+);
