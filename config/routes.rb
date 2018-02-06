9ade7b644523cc0f0b086b7535850fb2011c06ee
learnor <learnor@gmail.com>
2018-02-05 17:27:32 -0800

    add routes for news

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, default: { format: :json } do
     resource :session, only: %i(create destroy)
     resources :users, only: [:create]
+    resources :news, only: %i(index show)
   end
   root 'static_pages#root'
 end
